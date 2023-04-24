f0273d4232ffc1d371c130d0b4e2778016d4538c
Rosalyn304 <rosalynmemes@gmail.com>
2023-04-24 17:37:34 +0100

    Added basic entity

---
+++ practical_1/Main.cpp
@@ -0,0 +1,52 @@
+#include <SFML/Graphics.hpp>
+#include <Box2d/Box2d.h>
+#include <iostream>
+#include "LevelSystem.h"
+#include "player.h"
+
+using namespace std;
+using namespace sf;
+
+const int gameWidth = 1920;
+const int gameHeight = 1080;
+
+Texture roomSpriteSheet;
+
+void load()
+{
+
+    if (!roomSpriteSheet.loadFromFile("res/img/dungeon_tiles.png"))
+    {
+        cerr << "Failed to load spritesheet!" << endl;
+    }
+
+
+    ls::generateDungeon(1);
+}
+
+void Render(RenderWindow& window)
+{
+    ls::Render(window);
+}
+
+
+int main() {
+    RenderWindow window(sf::VideoMode({ gameWidth, gameHeight }), "Map Test");
+
+    load();
+
+    while (window.isOpen()) {
+        sf::Event event;
+
+        while (window.pollEvent(event)) {
+
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+        window.clear();
+        Render(window);
+        window.display();
+    }
+    return 0;
+}
